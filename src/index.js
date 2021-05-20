16e1862cf113c63887ab43fcbd088e8b83d669bc
Juan Cruz Deccechis <juandeccechis@gmail.com>
2021-05-19 21:05:29 -0300

    agergado de reducer/sagas

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//desde aca cambia por reducer/saga
+//import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from "react-redux";
+import reducer from "./reducer"; //despues va a cambiar por el composeReducer
+import rootSaga from './sagas'; //despues va a cambiar por un grupo de saga
+
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <React.StrictMode>
+  /*<React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,*/
+  //cambia porque ahora usa el store global
+  <Provider store={store}> 
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
+//reportWebVitals();
